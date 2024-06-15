6d6d3f161415900bdc937f5ca22ddcaec90c8e2c
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-06-15 18:42:58 -0500

    feat: CorsConfig

--- src/main/java/me/liga/Equipo/infrastructure/adapter/in/EquipoController.java
+++ src/main/java/me/liga/Equipo/infrastructure/adapter/in/EquipoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +16,20 @@ import me.liga.Equipo.domain.Equipo;
 @RestController // Envia json al front
 // http//localhost:8080/liga-app/
 @RequestMapping("liga-app")
-@CrossOrigin(value = "http//localhost:3000") // indica que los puerto de la app en front y back estan en diferentes puertos
+@CrossOrigin(origins = "http//localhost:3000") // indica que los puerto de la app en front y back estan en diferentes puertos
 public class EquipoController {
     private static final Logger logger = LoggerFactory.getLogger(EquipoController.class); // Esta variable permite enviar info a la consola
 
     @Autowired
     private IEquipoService equipoService;
-    // http://localhost:8080/liga-app/equipos
-    @GetMapping("/equipo")
+    // http://localhost:8080/liga-app/equipos // para postman http://127.0.0.1:8080/liga-app/equipo
+    @GetMapping(value = "/equipo", produces = "application/json")
     public List <Equipo> obtenerEquipos() {
         List<Equipo> equipos = equipoService.listarEquipos();
+        Equipo newEquipo = new Equipo(10l, "Equipo", 0, 0, 0, 0, 0, 0, 0);
+        equipos.add(newEquipo);
         equipos.forEach(equipo -> logger.info(equipo.toString()));
+
         return equipos;
     }
 }
